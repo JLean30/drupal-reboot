cccf9d692df127577fac756a210d7dbf26f9cc47
jlean30 <jlean30@protonmail.com>
2020-05-27 21:58:04 -0600

    tarea 29 de mayo

--- web/sites/default/settings.ddev.php
+++ web/sites/default/settings.ddev.php
@@ -14,8 +14,8 @@ $port = 3306;
 // so use the host-side bind port on docker IP
 if (empty(getenv('DDEV_PHP_VERSION') && getenv('IS_DDEV_PROJECT') == 'true')) {
   $host = "127.0.0.1";
-  $port = 32768;
-}
+  $port = 32772;
+} 
 
 $databases['default']['default'] = array(
   'database' => "db",
@@ -27,7 +27,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'MsgbIuazqpTzJIYzBxntbtBIYBISaurNMPgxlhckkHTHAZccMXSSMndbSNAdRrcv';
+$settings['hash_salt'] = 'ExvhlAYDSttZySzWkXvQotpCjAVlajSqYtqiqJOHJoYfaPyEJcMjqDTOUFDoNHYh';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
